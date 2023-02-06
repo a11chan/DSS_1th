d39619c1e5ba2b2dad3e5c387ccdf4a72227b431
allchan <a11chan@hotmail.com>
2023-02-06 16:22:03 +0900

    자바의 신 22장 예제 연습

--- src/main/java/GodOfJava/chap22/d/collection/ListSample.java
+++ src/main/java/GodOfJava/chap22/d/collection/ListSample.java
@@ -5,10 +5,109 @@ import java.util.ArrayList;
 public class ListSample {
   public static void main(String[] args) {
     ListSample sample = new ListSample();
-    sample.checkArrayList1();
+//    sample.checkArrayList1();
+//    sample.checkArrayList2();
+//    sample.checkArrayList5();
+//    sample.checkArrayList6();
+    sample.checkArrayList7();
+//    sample.checkArrayList8();
+//    sample.checkArrayList9();
   }
   public void checkArrayList1() {
-    ArrayList list1 = new ArrayList();
-    System.out.println("list1 = " + list1);
+    ArrayList<String> list = new ArrayList<>();
+    //list1.add(new Object());
+    list.add("arrayListSample");
+    //list1.add(new Double(1));
+    System.out.println("list = " + list);
+  }
+
+  public void checkArrayList2() {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    list.add("D");
+    list.add("E");
+    list.add(1,"A1");
+//    list.add(10,"A1");
+//    Exception in thread "main" java.lang.IndexOutOfBoundsException: Index: 10, Size: 5
+//    System.out.println("list = " + list);
+    for (String tempData : list) {
+      System.out.println("tempData = " + tempData);
+    }
+  }
+
+  public void checkArrayList3() {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    list.add("D");
+    list.add("E");
+    list.add(1,"A1");
+    ArrayList<String> list2 = new ArrayList<>();
+    list2.add("0 ");
+    list2.addAll(list);
+    for(String tempData:list2) {
+      System.out.println("List2 "+tempData);
+    }
+  }
+
+  public void checkArrayList5() {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    int listSize = list.size();
+    for(int loop = 0;loop<listSize;loop++){
+      System.out.println("list.get("+loop+") = " + list.get(loop));
+    }
+  }
+
+  public void checkArrayList6() {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    String[] strList = list.toArray(new String[0]);
+    System.out.println(strList[0]);
+  }
+
+  public void checkArrayList7() {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    String[] tempArray = new String[4];
+    String[] strList = list.toArray(tempArray);
+    for(String temp:strList) {
+      System.out.println(temp);
+    }
+  }
+
+  public void checkArrayList8() {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    list.add("A");
+//    System.out.println("list.remove(0) = " + list.remove(0));
+//    System.out.println(list.remove("A"));
+    ArrayList<String> temp = new ArrayList<>();
+    temp.add("A");
+    list.removeAll(temp);
+    for (int loopNum = 0; loopNum < list.size(); loopNum++) {
+      System.out.println("list.get("+loopNum+") = " + list.get(loopNum));
+    }
+  }
+
+  public void checkArrayList9() {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    list.add("A");
+    String result = list.set(3,"D");
+    System.out.println("result = " + result);
+    for (int loopNum = 0; loopNum < list.size(); loopNum++) {
+      System.out.println("list.get("+loopNum+") = " + list.get(loopNum));
+    }
   }
 }
